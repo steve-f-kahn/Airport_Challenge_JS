a8eaa87b5b5a5a0aef9b6f7cf5932bc7112f5caa
Steve Kahn <steve@kahn.org.uk>
2019-03-05 15:06:31 +0000

    Plane can takeoff

--- spec/PlaneSpec.js
+++ spec/PlaneSpec.js
@@ -11,4 +11,15 @@ describe('Plane', function() {
     expect(plane.land(airport)).toEqual("Airport");
   });
 
+  it("Is not flying when landed", function(){
+    plane.land(airport);
+    expect(plane.flying).toBe(false);
+  });
+
+  it('Takes off from the airport', function(){
+    plane.land(airport);
+    plane.takeoff(airport);
+    expect(plane.flying).toBe(true);
+  });
+
 });
--- src/Plane.js
+++ src/Plane.js
@@ -1,7 +1,13 @@
 var Plane = function(){
-
+  this.flying = true
 };
 
 Plane.prototype.land = function(airport) {
+  this.flying = false;
   return this._airport = airport;
+};
+
+Plane.prototype.takeoff = function(airport) {
+  this.flying = true;
+  this._airport = null;
 };
